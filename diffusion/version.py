63af9218b6699589f593cf08c2f0d57e0dba4538
Qisheng He <QishengRobertHe@outlook.com>
2023-09-12 15:04:50 -0400

    Version number updated

--- diffusion/version.py
+++ diffusion/version.py
@@ -1,5 +1,5 @@
 from torchmanager_core import Version
 
 API = Version("v0.1")
-CURRENT = Version(f"{API}.16a")
+CURRENT = Version(f"{API}.17a")
 DESCRIPTION = f"Torchmanager Implementation for Diffusion Model ({CURRENT})"
