807a3e6ee617a25516d08e2b2ebc54b82c3f5bdc
Qisheng He <Qisheng.He@wayne.edu>
2023-12-18 09:40:03 -0500

    v0.2.5b Build
    - Minor bugs fixed

--- diffusion/version.py
+++ diffusion/version.py
@@ -1,5 +1,5 @@
 from torchmanager_core import Version
 
-API = Version("v0.2.4")
-CURRENT = Version(f"{API}b")
+API = Version("v0.2")
+CURRENT = Version(f"{API}.5b")
 DESCRIPTION = f"Torchmanager Implementation for Diffusion Model ({CURRENT})"
