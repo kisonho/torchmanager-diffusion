72fbfa8afec7f4a88b0e6355433a57a7f918c36e
Qisheng He <Qisheng.He@wayne.edu>
2024-04-29 13:56:26 -0400

    Revert "04-29-24"
    This reverts commit ea23aea67046b4df444fd2c282ece64abe1dfe5b.

--- diffusion/nn/diffusion/diffusion.py
+++ diffusion/nn/diffusion/diffusion.py
@@ -1,5 +1,4 @@
 import abc, torch
-from enum import Enum
 from torchmanager_core import view
 from typing import Any, Generic, Optional, Sequence, TypeVar, Union, overload
 
@@ -34,18 +33,7 @@ class TimedModule(torch.nn.Module, abc.ABC):
         return NotImplemented
 
 
-Module = TypeVar('Module', bound=torch.nn.Module)
-
-
-class DiffusionMode(Enum):
-    """
-    The diffusion mode to use
-
-    * extends: `Enum`
-    """
-    FORWARD_DATA = 'forward_data'
-    FORWARD_DIFFUSION = 'forward_diffusion'
-    SAMPLING = 'sampling'
+Module = TypeVar('Module', bound=TimedModule)
 
 
 class DiffusionModule(torch.nn.Module, Generic[Module], abc.ABC):
@@ -75,35 +63,17 @@ class DiffusionModule(torch.nn.Module, Generic[Module], abc.ABC):
         self.model = model
         self.time_steps = time_steps
 
-    def __call__(self, *args: Any, mode: DiffusionMode = DiffusionMode.FORWARD_DATA, **kwds: Any) -> Any:
-        return super().__call__(*args, mode=mode, **kwds)
-
-    def forward(self, *args: Any, mode: DiffusionMode = DiffusionMode.FORWARD_DATA, **kwargs: Any) -> Any:
-        if mode == DiffusionMode.FORWARD_DATA:
-            return self.forward_data(*args, **kwargs)
-        elif mode == DiffusionMode.FORWARD_DIFFUSION:
-            return self.forward_diffusion(*args, **kwargs)
-        elif mode == DiffusionMode.SAMPLING:
-            return self.sampling_step(*args, **kwargs)
-        else:
-            raise NotImplementedError(f"Mode {mode} is not implemented for diffusion model")
-
-    def forward_data(self, data: DiffusionData, /) -> Any:
-        if isinstance(self.model, TimedModule):
-            return self.model(data)
-        elif data.condition is not None:
-            return self.model(data.x, data.t, condition=data.condition)
-        else:
-            return self.model(data.x, data.t)
+    def forward(self, data: DiffusionData, /) -> torch.Tensor:
+        return self.model(data)
 
     @abc.abstractmethod
-    def forward_diffusion(self, data: Any, /, condition: Optional[torch.Tensor] = None, t: Optional[torch.Tensor] = None) -> tuple[Any, torch.Tensor]:
+    def forward_diffusion(self, data: Any, t: torch.Tensor, /, condition: Optional[torch.Tensor] = None) -> tuple[Any, torch.Tensor]:
         """
         Forward pass of diffusion model, sample noises
 
         - Parameters:
             - data: Any kind of noised data
-            - t: An optional `torch.Tensor` of the time step, sampling uniformly if not given
+            - t: A `torch.Tensor` of the time step, sampling uniformly if not given
             - condition: An optional `torch.Tensor` of the condition to generate images
         - Returns: A `tuple` of noisy images and sampled time step in `DiffusionData` and noises in `torch.Tensor`
         """
@@ -136,7 +106,7 @@ class DiffusionModule(torch.nn.Module, Generic[Module], abc.ABC):
 
             # append to predicitions
             x = DiffusionData(imgs, t, condition=condition)
-            y = self(x, i, mode=DiffusionMode.SAMPLING)
+            y = self.sampling_step(x, i)
             imgs = y.to(imgs.device)
             progress_bar.update()
 
