a2dac7784f52050e418c93e03a3185836df40569
Qisheng He <Qisheng.He@wayne.edu>
2024-10-04 11:26:36 -0400

    v1.2b1 Build

--- diffusion/version.py
+++ diffusion/version.py
@@ -1,5 +1,5 @@
 from torchmanager_core import Version
 
 API = Version("v1.2")
-CURRENT = Version("v1.2a3")
+CURRENT = Version("v1.2b1")
 DESCRIPTION = f"Torchmanager Implementation for Diffusion Model ({CURRENT})"
