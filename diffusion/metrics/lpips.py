fbbf960c55d695f0a242a7a05dd4f62703135cdf
Qisheng Robert HE, PhD <Qisheng.He@hfhs.org>
2026-04-30 14:00:58 -0400

    Minor bugs fixed.

--- diffusion/metrics/lpips.py
+++ diffusion/metrics/lpips.py
@@ -1,4 +1,4 @@
-from torchmanager.metrics import Metric, LPIPS as _LPIPS, LPIPSNetType as LPIPSNet
+from torchmanager.metrics import LPIPS as _LPIPS, LPIPSNetType as LPIPSNet
 from torchmanager_core import torch
 from torchmanager_core.typing import Any, Protocol, cast
 from torchvision import models
@@ -8,6 +8,28 @@ class _FeatureExtractor(Protocol):
     @property
     def features(self) -> torch.nn.Sequential:
         ...
+
+
+def _load_lin_layers(checkpoint_name: str, channels: list[int]) -> torch.nn.ModuleList:
+    state_dict = torch.hub.load_state_dict_from_url(
+        f"https://raw.githubusercontent.com/richzhang/PerceptualSimilarity/master/lpips/weights/v0.1/{checkpoint_name}.pth",
+        file_name=f"lpips_{checkpoint_name}.pth",
+        map_location="cpu",
+        progress=False,
+    )
+
+    lin_layers = torch.nn.ModuleList([
+        torch.nn.Sequential(
+            torch.nn.Dropout(),
+            torch.nn.Conv2d(channel, 1, kernel_size=1, bias=False),
+        )
+        for channel in channels
+    ])
+    lin_layers.load_state_dict({
+        f"{i}.1.weight": state_dict[f"lin{i}.model.1.weight"]
+        for i in range(len(channels))
+    })
+    return lin_layers
 
 
 class LPIPS(_LPIPS):
@@ -29,14 +51,21 @@ class LPIPS(_LPIPS):
         match net:
             case LPIPSNet.ALEX:
                 feature_extractor = models.alexnet(weights=models.AlexNet_Weights.IMAGENET1K_V1)
+                checkpoint_name = "alex"
+                channels = [64, 192, 384, 256, 256]
             case LPIPSNet.VGG16:
                 feature_extractor = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1)
+                checkpoint_name = "vgg"
+                channels = [64, 128, 256, 512, 512]
             case LPIPSNet.SQUEEZE:
                 feature_extractor = models.squeezenet1_1(weights=models.SqueezeNet1_1_Weights.IMAGENET1K_V1)
+                checkpoint_name = "squeeze"
+                channels = [64, 128, 256, 384, 384, 512, 512]
+        lin_layers = _load_lin_layers(checkpoint_name, channels)
 
         # initialize LPIPS
         feature_extractor = cast(_FeatureExtractor, feature_extractor)
-        super().__init__(feature_extractor=feature_extractor.features, net_type=net, target=target)
+        super().__init__(feature_extractor=feature_extractor.features, net_type=net, lin_layers=lin_layers, target=target)
 
     def forward_features(self, x: torch.Tensor) -> Any:
         # check channels
